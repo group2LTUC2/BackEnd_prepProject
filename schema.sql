0a5a8776464c6989ba412ad91af03f66ae731899
abostaf <23037172@student.ltuc.com>
2023-03-24 19:24:19 +0000

    Back-End done

--- schema.sql
+++ schema.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS donationCard;
-
 CREATE TABLE If NOT EXISTS donationCard (
     id SERIAL PRIMARY KEY,
     fullName VARCHAR(255),
@@ -7,10 +6,8 @@ CREATE TABLE If NOT EXISTS donationCard (
     item VARCHAR(255),
     quantity VARCHAR(255),
     locationOf VARCHAR(255),
-    img VARCHAR(255)
+    img IMAGE
 );
-
-
 CREATE TABLE if NOT EXISTS volunteers (
     id SERIAL PRIMARY KEY,
     fullName VARCHAR(255),
