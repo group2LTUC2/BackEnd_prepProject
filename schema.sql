e5c20a796dad89080a805b2a264b68150fc52618
abostaf <23037172@student.ltuc.com>
2023-03-26 21:51:04 +0000

    Back-End Done

--- schema.sql
+++ schema.sql
@@ -1,18 +1,40 @@
+-- DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS donationCard;
+DROP TABLE IF EXISTS volunteers;
+DROP TABLE IF EXISTS takenDonations;
+
+
 CREATE TABLE If NOT EXISTS donationCard (
-    id SERIAL PRIMARY KEY,
+    donationCard_id SERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL REFERENCES users(email),
     fullName VARCHAR(255),
     phonNumber VARCHAR(255),
     item VARCHAR(255),
     quantity VARCHAR(255),
     locationOf VARCHAR(255),
-    img IMAGE
+    img VARCHAR(255)
 );
+
 CREATE TABLE if NOT EXISTS volunteers (
     id SERIAL PRIMARY KEY,
-    fullName VARCHAR(255),
+    fullName VARCHAR(255)NOT NULL REFERENCES users(email),
     phonNumber VARCHAR(255),
     img VARCHAR(255),
     locationOf VARCHAR(255)
-)
+);
 
+CREATE TABLE if NOT EXISTS takenDonations (
+    takenDonations_id SERIAL PRIMARY KEY,
+    fullName VARCHAR(255)NOT NULL REFERENCES users(email),
+    phonNumber VARCHAR(255),
+    locationOf VARCHAR(255)
+);
+
+
+CREATE TABLE If NOT EXISTS users  (
+   id SERIAL PRIMARY KEY,
+   username VARCHAR(50) UNIQUE NOT NULL,
+   email VARCHAR(50) UNIQUE NOT NULL,
+    img VARCHAR(1000) NOT NULL
+
+);
--- server.js
+++ server.js
@@ -15,10 +15,14 @@ server.get("/donations", donationsHandler); // donation page ---> show all
 server.post("/donations", addHandler);
 server.put("/donations/:id", updateDonationHandler);
 server.delete("/donations/:id", deleteDonationCardHandler);
-server.get('/volunteer', getdashboardHandler) // render all volunteers
+server.get('/volunteer', getdashboardHandler); // render all volunteers
 server.post('/volunteer', addVolunteerHandler);
-server.delete('/volunteer/:id',deleteVolunteerHandler)
+server.delete('/volunteer/:id',deleteVolunteerHandler);
+server.post('/user',addUserHandler);
+server.get('/user',getUserHandler);
 server.get('/api',apihandler);
+server.post('/takenitems', postTakenItemsHandler);// inserting taken item into database(takenDonations)server.get('/api',apihandler);
+server.get('/takenitems', getTakenItemsHandler);// getting taken items from database(takenDonations)
 server.get('*', notFoundHandler);
 server.use(errorHandler);
 // handlers
@@ -37,14 +41,15 @@ function donationsHandler(req, res) {//in Donation page
 //Mustafa
 function addHandler(req, res) {
   const cadres = req.body;
-  const sql = `INSERT INTO donationCard (fullName,phonNumber,item,quantity,locationOf,img) VALUES($1,$2,$3,$4,$5,$6) RETURNING *`;
+  const sql = `INSERT INTO donationCard (email,fullName,phonNumber,item,quantity,locationOf,img) VALUES($1,$2,$3,$4,$5,$6,$7) RETURNING *`;
   const values = [
+    cadres.email,
     cadres.fullName,
     cadres.phonNumber,
     cadres.item,
     cadres.quantity,
     cadres.locationOf,
-    cadres.img,
+    cadres.img
   ];
   client.query(sql, values)
     .then((result) => {
@@ -67,7 +72,7 @@ function addHandler(req, res) {
 function updateDonationHandler(req, res) {//in donation page
   const id = req.params.id;
   const donate = req.body;
-  const sql = `UPDATE donationCard SET fullName='${donate.fullName}' WHERE id= ${id} RETURNING *;`;
+  const sql = `UPDATE donationCard SET fullName='${donate.quantity}' WHERE id= ${id} RETURNING *;`;
   client.query(sql)
     .then((data) => {
       const sql = `SELECT * FROM donationCard`;
@@ -108,24 +113,49 @@ function deleteDonationCardHandler(req,res){// in Donation Page
 }
 //Sahm
 function apihandler(req,res){
-  const options = {
-    method: 'GET',
-    url: 'https://aladhan.p.rapidapi.com/timingsByCity',
-    params: {country: 'Jordan', city: 'Amman',method: '5', state: 'Amman'},
-    headers: {
-      'X-RapidAPI-Key': '3431701e17msh88fc56a55b413d0p19048djsnab34a8eaed3d',
-      'X-RapidAPI-Host': 'aladhan.p.rapidapi.com'
-    }
-  };
-  
-  axios.request(options).then(function (response) {
-    console.log(response.data);
-    res.send([response.data.data.timings,response.data.data.date.hijri.date+" هجري",response.data.data.date.gregorian.date+" ميلادي"]);
-  }).catch(function (error) {
-    console.error(error);
-  });
+  try {
+    // const apiKey = process.env.APIKey;
+    const url = `https://api.aladhan.com/v1/timingsByAddress/26-03-2023?address=Amman%2C+JORDAN&method=99&methodSettings=18.5%2Cnull%2C17.5`;
+    axios.get(url)
+      .then((result2) => {
+        // let mapRes = `${result2.data.data.timings.Maghrib}`;
+        // let map2 =`${['Date '+result2.data.data.date.readable+ ' ' +result2.data.data.timings.Maghrib+" موعد صلاة المغرب"]}`
+        let map2 =`${'Date '+result2.data.data.date.readable+ ' ' +result2.data.data.timings.Maghrib+" موعد صلاة المغرب"}`
+
+        res.send(map2);
+        console.log(map2)
+      })
+      .catch((error) => {
+        res.status(500).send(error);
+      });
+  } catch (error) {
+    errorHandler(error, req, res);
+  }
+
 }
-//Sahm
+  // const axios = require("axios");
+
+  // const options = {
+  //   method: 'GET',
+  //   url: 'https://muslimsalat.p.rapidapi.com/Zarqa.json',
+  //   headers: {
+  //     'X-RapidAPI-Key': '6ecec3d108msh4b17ce9d2e0f1b3p1d9c64jsnda6204e4f17c',
+  //     'X-RapidAPI-Host': 'muslimsalat.p.rapidapi.com'
+  //   }
+  // };
+  
+  // axios.request(options).then(function (response) {
+  //   console.log(response.data.items);
+  //   res.send(response.data.items)
+
+    // res.send([response.data.datatimings,response.data.hijri.date+" هجري",response.data.gregorian.date+" ميلادي"]);
+
+
+//   }).catch(function (error) {
+//     console.error(error);
+//   });
+
+// }
 function getdashboardHandler(req, res){
 
   const sql = `SELECT * FROM volunteers`;
@@ -139,6 +169,30 @@ function getdashboardHandler(req, res){
   })
 
 }
+function addUserHandler(req,res){
+  const cadres = req.body;
+  let sql = `INSERT INTO users(username,email,img) VALUES ('${cadres.username}','${cadres.email}','${cadres.img}') RETURNING *;`;
+  client.query(sql)
+    .then((data) => {
+      res.send(data.rows);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+}
+
+function getUserHandler(req,res){
+  const sql = `SELECT * FROM users`;
+  client.query(sql)
+    .then((Result) => {
+      res.send(Result.rows);
+    })
+    .catch((error) => {
+      errorHandler(error, req, res);
+    });
+
+}
+
 //Toqa
 function addVolunteerHandler(req, res){
 
@@ -183,6 +237,46 @@ function deleteVolunteerHandler(req,res){
       res.status(500).send(error);
     });
 }
+//new routes added by ahmad
+function getTakenItemsHandler(req, res) {
+  const sql = `SELECT * FROM takenDonations`;
+  client.query(sql)
+    .then((Result) => {
+      res.send(Result.rows);
+    })
+    .catch((error) => {
+      errorHandler(error, req, res);
+    });
+}
+
+function postTakenItemsHandler(req, res) {
+  const cadres = req.body;
+  const sql = `INSERT INTO takenDonations (fullName,phonNumber,locationOf) VALUES($1,$2,$3) RETURNING *`;
+  const values = [
+    cadres.fullName,
+    cadres.phonNumber,
+    cadres.locationOf
+  ];
+  client.query(sql, values)
+    .then((result) => {
+      const sql = "SELECT * FROM takenDonations";
+      client.query(sql) //the data come from query from client data base stored in (.then( inside data))
+        .then((data) => {
+          res.send(data.rows);
+        })
+        .catch((error) => {
+          res.send(error);
+        });
+
+
+    })
+    .catch((error) => {
+      errorHandler(error, req, res);
+    });
+
+}
+//end of new routers
+
 // Mohammed
 function notFoundHandler(req,res){
   res.status(404).send("Page not found");
