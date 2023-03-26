fc484cffaf998ae7abaa7d3c1a70ab6780ae51ea
toqawasfi <toqawasfi123@gmail.com>
2023-03-26 11:26:43 +0000

    schema done

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS takenDonations;
 DROP TABLE IF EXISTS volunteers;
 
 CREATE TABLE If NOT EXISTS donationCard (
-    id SERIAL PRIMARY KEY,
+    donationCard_id SERIAL PRIMARY KEY,
     fullName VARCHAR(255),
     phonNumber VARCHAR(255),
     item VARCHAR(255),
@@ -21,17 +21,13 @@ CREATE TABLE if NOT EXISTS volunteers (
 );
 
 CREATE TABLE if NOT EXISTS takenDonations (
-    id SERIAL PRIMARY KEY,
+    takenDonations_id SERIAL PRIMARY KEY,
     fullName VARCHAR(255),
     phonNumber VARCHAR(255),
     locationOf VARCHAR(255),
-    
-    donerName VARCHAR(255),
-    donerLocation VARCHAR (255),
-    donerPhone VARCHAR(255),
+     donationCard_id SERIAL
+    FOREIGN KEY ( donationCard_id) REFERENCES donationCard (donationCard_id)
 
-    item VARCHAR(255),
-    quantity VARCHAR(255)
 
 );
 
