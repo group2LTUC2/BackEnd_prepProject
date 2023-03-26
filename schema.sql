c251bdca86b989656c6276390e093bf40b922342
ahmaderaqi <ahmadzaher573@gmail.com>
2023-03-26 11:02:10 +0000

    ahmadzaher

--- schema.sql
+++ schema.sql
@@ -1,4 +1,7 @@
 DROP TABLE IF EXISTS donationCard;
+DROP TABLE IF EXISTS takenDonations;
+DROP TABLE IF EXISTS volunteers;
+
 CREATE TABLE If NOT EXISTS donationCard (
     id SERIAL PRIMARY KEY,
     fullName VARCHAR(255),
@@ -6,13 +9,33 @@ CREATE TABLE If NOT EXISTS donationCard (
     item VARCHAR(255),
     quantity VARCHAR(255),
     locationOf VARCHAR(255),
-    img IMAGE
+    img VARCHAR(255)
 );
+
 CREATE TABLE if NOT EXISTS volunteers (
     id SERIAL PRIMARY KEY,
     fullName VARCHAR(255),
     phonNumber VARCHAR(255),
     img VARCHAR(255),
     locationOf VARCHAR(255)
-)
+);
+
+CREATE TABLE if NOT EXISTS takenDonations (
+    id SERIAL PRIMARY KEY,
+    fullName VARCHAR(255),
+    phonNumber VARCHAR(255),
+    locationOf VARCHAR(255),
+    
+    donerName VARCHAR(255),
+    donerLocation VARCHAR (255),
+    donerPhone VARCHAR(255),
+
+    item VARCHAR(255),
+    quantity VARCHAR(255)
+
+);
+
+
+
+
 
--- server.js
+++ server.js
@@ -7,18 +7,25 @@ require("dotenv").config();
 const server = express();
 server.use(cors());
 server.use(express.json());
-const PORT = process.env.PORT || 3000;
+const PORT = 3003;
 const client = new pg.Client(process.env.DATABASE_URL);
 //Route's:
-// http://localhost:3000
+// http://localhost:3003
 server.get("/donations", donationsHandler); // donation page ---> show all
 server.post("/donations", addHandler);
 server.put("/donations/:id", updateDonationHandler);
 server.delete("/donations/:id", deleteDonationCardHandler);
 server.get('/volunteer', getdashboardHandler) // render all volunteers
 server.post('/volunteer', addVolunteerHandler);
-server.delete('/volunteer/:id',deleteVolunteerHandler)
-server.get('/api',apihandler);
+server.delete('/volunteer/:id', deleteVolunteerHandler)
+
+server.get('/takenitems', getTakenItemsHandler);// getting taken items from database(takenDonations)
+server.post('/takenitems', postTakenItemsHandler);// inserting taken item into database(takenDonations)
+
+server.get('/api', apihandler);
+
+
+
 server.get('*', notFoundHandler);
 server.use(errorHandler);
 // handlers
@@ -50,12 +57,12 @@ function addHandler(req, res) {
     .then((result) => {
       const sql = "SELECT * FROM donationCard";
       client.query(sql) //the data come from query from client data base stored in (.then( inside data))
-      .then((data) => {
-        res.send(data.rows);
-      })
-      .catch((error) => {
-        res.send(error);
-      });
+        .then((data) => {
+          res.send(data.rows);
+        })
+        .catch((error) => {
+          res.send(error);
+        });
 
 
     })
@@ -67,17 +74,18 @@ function addHandler(req, res) {
 function updateDonationHandler(req, res) {//in donation page
   const id = req.params.id;
   const donate = req.body;
-  const sql = `UPDATE donationCard SET fullName='${donate.fullName}' WHERE id= ${id} RETURNING *;`;
-  client.query(sql)
+  const sql = `UPDATE donationCard SET quantity=$1 WHERE id=${id} RETURNING *`;
+  const value = [donate.quantity];
+  client.query(sql, value)
     .then((data) => {
       const sql = `SELECT * FROM donationCard`;
       client.query(sql) //the data come from query from client data base stored in (.then( inside data))
-      .then((data) => {
-        res.status(200).send(data.rows);
-      })
-      .catch((error) => {
-        res.send(error);
-      });
+        .then((data) => {
+          res.status(200).send(data.rows);
+        })
+        .catch((error) => {
+          res.send(error);
+        });
 
     })
     .catch((error) => {
@@ -85,7 +93,7 @@ function updateDonationHandler(req, res) {//in donation page
     });
 }
 //Ahmad
-function deleteDonationCardHandler(req,res){// in Donation Page
+function deleteDonationCardHandler(req, res) {// in Donation Page
   const id = req.params.id;
   const sql = `DELETE FROM donationCard WHERE id=${id} RETURNING *;`;
   client.query(sql)
@@ -93,12 +101,12 @@ function deleteDonationCardHandler(req,res){// in Donation Page
       // res.send(data.rows);
       const sql = `SELECT * FROM donationCard`;
       client.query(sql) //the data come from query from client data base stored in (.then( inside data))
-      .then((data) => {
-        res.status(200).send(data.rows);
-      })
-      .catch((error) => {
-        res.send(error);
-      });
+        .then((data) => {
+          res.status(200).send(data.rows);
+        })
+        .catch((error) => {
+          res.send(error);
+        });
 
     })
     .catch((error) => {
@@ -107,40 +115,40 @@ function deleteDonationCardHandler(req,res){// in Donation Page
 
 }
 //Sahm
-function apihandler(req,res){
+function apihandler(req, res) {
   const options = {
     method: 'GET',
     url: 'https://aladhan.p.rapidapi.com/timingsByCity',
-    params: {country: 'Jordan', city: 'Amman',method: '5', state: 'Amman'},
+    params: { country: 'Jordan', city: 'Amman', method: '5', state: 'Amman' },
     headers: {
       'X-RapidAPI-Key': '3431701e17msh88fc56a55b413d0p19048djsnab34a8eaed3d',
       'X-RapidAPI-Host': 'aladhan.p.rapidapi.com'
     }
   };
-  
+
   axios.request(options).then(function (response) {
     console.log(response.data);
-    res.send([response.data.data.timings,response.data.data.date.hijri.date+" هجري",response.data.data.date.gregorian.date+" ميلادي"]);
+    res.send([response.data.data.timings, response.data.data.date.hijri.date + " هجري", response.data.data.date.gregorian.date + " ميلادي"]);
   }).catch(function (error) {
     console.error(error);
   });
 }
 //Sahm
-function getdashboardHandler(req, res){
+function getdashboardHandler(req, res) {
 
   const sql = `SELECT * FROM volunteers`;
 
   client.query(sql)
-  .then((Result)=>{
-       res.send(Result.rows)
+    .then((Result) => {
+      res.send(Result.rows)
 
-  }).catch((error)=>{
-       errorHandler(error, req, res); 
-  })
+    }).catch((error) => {
+      errorHandler(error, req, res);
+    })
 
 }
 //Toqa
-function addVolunteerHandler(req, res){
+function addVolunteerHandler(req, res) {
 
   const volunteers = req.body;
   const sql = `INSERT INTO volunteers (fullName,phonNumber,img,locationOf) VALUES($1,$2,$3,$4) RETURNING *`;
@@ -148,22 +156,22 @@ function addVolunteerHandler(req, res){
   const values = [volunteers.fullName, volunteers.phonNumber, volunteers.img, volunteers.locationOf];
 
   client.query(sql, values)
-  .then((result) => {
-    const sql = `SELECT * FROM volunteers`;
-    client.query(sql) //the data come from query from client data base stored in (.then( inside data))
-    .then((data) => {
-      res.status(200).send(data.rows);
+    .then((result) => {
+      const sql = `SELECT * FROM volunteers`;
+      client.query(sql) //the data come from query from client data base stored in (.then( inside data))
+        .then((data) => {
+          res.status(200).send(data.rows);
+        })
+        .catch((error) => {
+          res.send(error);
+        });
     })
     .catch((error) => {
-      res.send(error);
-    });
-  })
-  .catch((error) => {
       errorHandler(error, req, res);
-  })
+    })
 }
 //Toqa
-function deleteVolunteerHandler(req,res){
+function deleteVolunteerHandler(req, res) {
   const id = req.params.id;
   const sql = `DELETE FROM volunteers WHERE id=${id} RETURNING *;`;
   client.query(sql)
@@ -171,27 +179,75 @@ function deleteVolunteerHandler(req,res){
       // res.send(data.rows);
       const sql = `SELECT * FROM volunteers`;
       client.query(sql) //the data come from query from client data base stored in (.then( inside data))
-      .then((data) => {
-        res.status(200).send(data.rows);
-      })
-      .catch((error) => {
-        res.send(error);
-      });
+        .then((data) => {
+          res.status(200).send(data.rows);
+        })
+        .catch((error) => {
+          res.send(error);
+        });
 
     })
     .catch((error) => {
       res.status(500).send(error);
     });
 }
+
+//new routes added by ahmad
+function getTakenItemsHandler(req, res) {
+  const sql = `SELECT * FROM takenDonations`;
+  client.query(sql)
+    .then((Result) => {
+      res.send(Result.rows);
+    })
+    .catch((error) => {
+      errorHandler(error, req, res);
+    });
+}
+
+function postTakenItemsHandler(req, res) {
+  const cadres = req.body;
+  const sql = `INSERT INTO takenDonations (fullName,phonNumber,item,quantity,locationOf,donerName,donerLocation,donerPhone)
+                 VALUES($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *`;
+  const values = [
+    cadres.fullName,
+    cadres.phonNumber,
+    cadres.item,
+    cadres.quantity,
+    cadres.locationOf,
+    cadres.donerName,
+    cadres.donerLocation,
+    cadres.donerPhone
+
+  ];
+  client.query(sql, values)
+    .then((result) => {
+      const sql = "SELECT * FROM donationCard";
+      client.query(sql) //the data come from query from client data base stored in (.then( inside data))
+        .then((data) => {
+          res.send(data.rows);
+        })
+        .catch((error) => {
+          res.send(error);
+        });
+
+
+    })
+    .catch((error) => {
+      errorHandler(error, req, res);
+    });
+
+}
+//end of new routers
+
 // Mohammed
-function notFoundHandler(req,res){
+function notFoundHandler(req, res) {
   res.status(404).send("Page not found");
 }
 // Mohammad
 function errorHandler(error, req, res) {
   const err = {
-      status: 500,
-      message: error
+    status: 500,
+    message: error
   }
   res.send(err);
 }
